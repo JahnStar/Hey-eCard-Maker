6ac17b49dd9ab11a6d7f4c375eb67c4e25091f66
Halil Emre Yildiz <halilemreyildiz@gmail.com>
2021-05-05 12:19:55 +0300

    Version 2
    New End-to-End Encrypted eCard Creation System With Fully Customizable Modern Templates

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
-# Happy-Birthday
+# Hey eCard Maker: Create Free Online Card!
 
-Demo:
-https://jahnstar.github.io/happy-birthday/?name=Jahn%20Star
-https://jahnstar.github.io/happy-birthday/?id=0
+<p align="center">
+    <a href="https://github.com/JahnStar/hey-ecard-maker/blob/master/LICENSE" alt="License: MIT"><img src="https://img.shields.io/badge/License-MIT-brightgreen.svg" />
+    <a href="https://jahnstar.github.io/donate/" target="_blank" rel="noopener" noreferrer alt="Support the Developer">
+    <img src="https://img.shields.io/badge/Donate-☕-orange.svg"/></a></a>
+  
+<p align="center"><b>E-cards are a fun and easy way to send a message!</b></p><p align="center">
+Quickly create and customize your free online cards. And and send to a friend today! (Free & Safe)</p>
+
+<p align="center">
+  <a alt="Developed by Jahn Star" href="https://www.instagram.com/jahn_star/"><b>Developed by Halil Emre Yildiz</b></a>
+</p>
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Halil Emre Yildiz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/script.js
+++ src/script.js
@@ -1,62 +1,99 @@
 // <!--  Author: Halil Emre Yildiz - </> by @JahnStar -->
-$(function(){ 
-	$.getJSON('src/data.json', function(data){
-		var index = -1;
-		var name = window.location.href;
-		if (name.includes("?name="))
-		{
-			name = decodeURI(name.split("?name=")[1]);
-			$('.button').html(name);
-			scrollToMiddle("#cake");
-		}
-		else if (name.includes("?id="))
-		{
-			index = decodeURI(name.split("?id=")[1]);
-			
-			var firstName = data[index].firstName;
-			var lastName = data[index].lastName;
-			var dobDate = data[index].dobDate;
-			var pictureUrl = data[index].pictureUrl;
-			var nat = data[index].nat;
-			var message = data[index].message;
-			var message1 = data[index].message1;
-				
-			name = firstName + " " + lastName;
-			if (message1.includes("$age"))
-			{
-				dobDate = new Date(dobDate);
-				var age = new Date(Date.now() - dobDate.getTime()); 
-				age = Math.abs(age.getUTCFullYear() - 1970);
-				
-				message1 = message1.replace("$age", age);
-			}
-			var message2 = data[index].message2;
-			var message3 = data[index].message3;
-			
-			$('#title').html(firstName + " " + lastName);
-			$('#message').html(message);
-			$('.button').html(message1);
-			$(".circle").attr("src", pictureUrl);
-		}
+var editMode;
+function EditMode(){
+	editMode = !editMode;
+	if (editMode){
+		$("#mode").attr('class', 'fa fa-eye');
+		$('.EditMode').show();
+		$('.ViewMode').hide();
+	$("#edit input:text").first().focus();
+	}
+	else {
+		$("#mode").attr('class', 'fa fa-pencil');
+		$('.EditMode').hide();
+		$('.ViewMode').show();
+	}
+	Save();
+	Load();
+}
+function Load(){
+	$("#title_").attr('value', data.title);
+	$("#message_").html(data.message);
+	$("#pictureUrl_").attr('value', data.pictureUrl);
+	
+	$("#title").html(data.title.replace(new RegExp("script>", "g"), ''));
+	$("#message").html(data.message.replace(new RegExp("script>", "g"), '').replace(new RegExp("＜", "g"), '<'));
+	$('#templates').val(data.template);
+	$("#template").attr("src", "src/media/" + data.template + ".png");
+	$("#picture").attr("src", data.pictureUrl);
+	
+	var mainColor = data.mainColor;
+	var bgColor = data.bgColor;
+	$("#mainColor_").attr('value', mainColor);
+	$("#bgColor_").attr('value', bgColor);
+	if (data.suprise) $("#suprise").attr("checked", "checked");
+	else $(".suprise").hide();
+
+	$("h1").css("color", mainColor);
+	$("h1 input").css("color", mainColor);
+	$(".changeMode .fa").css("background", mainColor);
+	$("body").css("background", "radial-gradient(at center center, #fff -10%, " + mainColor + " 125%)");
+	$(".button").css("border", mainColor);
+	$(".button").css("background", mainColor);
+	$(".circle").css("border", "5px solid" + mainColor);
+	$(".circle").css("color", mainColor);
+	$(".circle").css("background", bgColor);
+	$(".card").css("border-top", "5px solid " + mainColor);
+	$(".card").css("border-bottom", "5px solid " + mainColor);
+	$(".card").css("background", bgColor);
+	$("#alert").css("color", mainColor);
+	$("#alert").css("background", bgColor);
+}
+function Save(stringData){
+	if(stringData){	
+		stringData = jQuery.parseJSON(stringData);
+		data = stringData;
+	}
+	else {
+		data.title = $("#title_").val();
+		data.message = $("#message_").val();
+		data.template = $("#templates").val();
 		
-		if (index > -1)
-		{
-			var fontColor = data[index].fontColor;
-			var bgColor = data[index].bgColor;
-			
-			$("h1").css("color", fontColor);
-			$("body").css("background", "radial-gradient(ellipse at center, #fff -10%, " + fontColor + " 200%)");
-			$(".circle").css("border", "5px solid" + fontColor);
-			$(".button").css("background", fontColor);
-			$(".button").css("border", fontColor);
-			$(".card").css("border-top", "5px solid " + fontColor);
-			$(".card").css("border-bottom", "5px solid " + fontColor);
-			$(".card").css("background", bgColor);
+		var imgUrl = $("#pictureUrl_").val();
+		if (imgUrl.slice(-3) == "png" || imgUrl.slice(-3) == "jpg") {
+			$('#picture').show();
+			data.pictureUrl = imgUrl;
 		}
+		else $('#picture').hide();
+		
+		data.mainColor = $("#mainColor_").val();
+		data.bgColor = $("#bgColor_").val();
+		data.suprise = $("#suprise").is(':checked');
+	}
+}
+function setLanguage(){
+	var language = 1;
+	if (navigator.language == "tr-TR" || navigator.language == "tr") language = 0;
+
+	$("#alert").html($("#alert").html().split("$")[language]);
+	$(".button.a2a_dd").html($(".button.a2a_dd").html().split("$")[language]);
+	$("#templates option").each(function () {
+        $(this).html($(this).html().split("$")[language]);
+		$(this).parent().attr('label',$(this).parent().attr('label').split("$")[language]);
 	});
-});
-function Notification() { alert("İyi Ki Doğdun " + name + " :)"); }
-function scrollToMiddle(id) {
+	$("#templates-data option").each(function () {
+        $(this).html($(this).html().split("$")[language]);
+	});
+}
+function loadTemplate(){
+	var template = $('#templates-data option').eq($("#templates")[0].selectedIndex);
+	$("#title_").val(template.text().split('*')[0]);
+	$("#message_").val(template.text().split('*')[1]);
+	var colors = template.val().split('#');
+	$("#mainColor_").val('#' + colors[0]);
+	$("#bgColor_").val('#' + colors[1]);
+}
+function scrollToMiddle(id){
 
     var elem_position = $(id).offset().top;
     var window_height = $(window).height();
@@ -64,6 +101,85 @@ function scrollToMiddle(id) {
 
     window.scrollTo(0,y);
 }
-setTimeout(function() {
+function copyToClipboard(text){
+  var input = document.body.appendChild(document.createElement("input"));
+  input.value = text;
+  input.focus();
+  input.select();
+  document.execCommand('copy');
+  input.parentNode.removeChild(input);
+}
+function copyLink(share_data){
+try{
+	$(".a2a_full_footer").html("<h2> Hey eCard Maker: Create Free Online Card! <br><br> - <i class='fa fa-link' style='color:cyan;'></i> Copied! - </h2>");
+	Save();
+	Load();
+	
+	var dataHex = window.btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+	dataHex = encodeURI(location.protocol + '//' + location.host + location.pathname + '?hey=' + dataHex);
+	copyToClipboard(dataHex);
+	
+	// a2a_config.linkname = data.title;
+	// a2a_config.linkurl = dataHex;
+	// a2a.init('page');
+	return { title: data.title, url: dataHex };
+}
+catch(e){
+	$(".a2a_full_footer").html("<h2> -<i class='fa fa-exclamation-triangle' style='color:red;'></i> Failed! - </h2>");
+
+	$("#mode").attr('class', 'fa fa-eye');
+	$('.EditMode').show();
+	$('.ViewMode').hide();
+	$("#edit input:text").first().focus();
+	}
+}
+function playMusic()
+{
+	var player = document.getElementById("player"); 
+	if(player.currentTime > 0) 
+	{
+		player.pause();
+		player.currentTime=0;
+	}
+	else player.play();
+	setTimeout(function(){
+	$('#alert').fadeOut(1000);
+	}, 5000);
+}
+setTimeout(function(){
     $('#canvas').fadeOut(1000);
+	$('#alert').fadeOut(1000);
 }, 15000);
+$(function(){
+	setLanguage();
+	var getData = decodeURI(window.location.href.split('?hey=')[1]);
+	if (!getData || getData === 'undefined')
+	{
+		$("#suprise").attr("checked", "checked");
+		loadTemplate();
+		Save();
+	}
+	else Save(decodeURIComponent(escape(window.atob(getData))));
+	Load();
+	
+	$("#templates").on('change', function(){
+		loadTemplate();
+		
+		Save();
+		Load();
+	});
+	
+	document.querySelectorAll('input[type=color]').forEach(function(picker) {
+	  picker.addEventListener('change', function() {
+		$("#" + picker.id).attr("value", picker.value);
+		
+		Save();
+		Load();
+		});
+	});
+	
+	$(".a2a_dd").click(copyLink);
+});
+var data = new Object();
+window.addEventListener('load', function () {
+})
